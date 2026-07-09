91cd8bb7a1eadba700c36d24cf6a380244dcab69
Fetena <117965359+Fetena@users.noreply.github.com>
2026-07-09 10:50:18 +0300

    Create firebase.js

---
+++ Public/firebase.js
@@ -0,0 +1,12 @@
+// firebase.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  // Your Firebase project credentials go here
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
