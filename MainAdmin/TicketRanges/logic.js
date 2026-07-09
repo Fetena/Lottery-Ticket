023a94eb3d468a87dee1f0a4e6026e5641e2217c
Fetena <117965359+Fetena@users.noreply.github.com>
2026-07-09 11:17:42 +0300

    Update logic.js

--- MainAdmin/TicketRanges/logic.js
+++ MainAdmin/TicketRanges/logic.js
@@ -1,7 +1,6 @@
-// /MainAdmin/TicketRanges/logic.js
 import { db } from '../../firebase.js';
-import { doc, updateDoc } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+import { collection, getDocs } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
-export async function updateRange(adminEmail, start, end) {
-    await updateDoc(doc(db, "admins", adminEmail), { start, end }); //[cite: 3]
+export async function getAllTickets() {
+    return await getDocs(collection(db, "tickets")); //[cite: 3]
 }
